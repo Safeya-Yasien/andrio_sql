e06eb8807dc9816d5c6530e7b3bb62cd957e5303
Safeya-Yasien <bluesky7yas@gmail.com>
2023-12-20 20:24:52 +0200

    connect with sql server db

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation(files("C:\\Users\\DUBAI\\Documents\\connect_android_sql\\app\\libs\\jtds-1.3.1.jar"))
+//    implementation(files("C:\\Users\\DUBAI\\Documents\\connect_android_sql\\app\\libs\\jtds-1.3.1.jar"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("io.reactivex.rxjava3:rxjava:3.1.1")
+    implementation("net.sourceforge.jtds:jtds:1.3.1")
 }
--- app/src/main/java/com/example/connect_android_sql/MainActivity.java
+++ app/src/main/java/com/example/connect_android_sql/MainActivity.java
@@ -1,48 +1,79 @@
 package com.example.connect_android_sql;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+
+import io.reactivex.rxjava3.disposables.Disposable;
+import kotlinx.coroutines.Job;
 
 public class MainActivity extends AppCompatActivity {
 
-    Connection connect;
-    String ConnectionResult = "";
+    private TextView textView;
+
+    private static String ip = "192.168.1.4";
+    private static String port = "1433";
+    private static String Classes = "net.sourceforge.jtds.jdbc.Driver";
+    private static String database = "testing";
+    private static String username = "test4";
+    private static String password = "test";
+    private static String url = "jdbc:jtds:sqlserver://"+ip+":"+port+"/"+database;
+
+    private Connection connection = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
+        textView = findViewById(R.id.textView);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            Class.forName(Classes);
+            connection = DriverManager.getConnection(url, username,password);
+            textView.setText("SUCCESS");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            textView.setText("ERROR");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            textView.setText("FAILURE");
+        }
     }
 
-    void GetTextFromSql(View v){
-        TextView tx1 = (TextView) findViewById(R.id.textView);
-        TextView tx2 = (TextView) findViewById(R.id.textView2);
-
-        try{
-        ConnectionHelper connectionHelper = new ConnectionHelper();
-        connect = connectionHelper.connectionclass();
-        if(connect != null){
-            String query = "Select * from customer";
-            Statement st = connect.createStatement();
-            ResultSet rs = st.executeQuery(query);
-
-            while(rs.next()){
-                tx1.setText(rs.getString(1));
-                tx2.setText(rs.getString(2));
+    public void sqlButton(View view) {
+        if (connection!=null){
+            Statement statement = null;
+            try {
+                statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("Select * from person;");
+                while (resultSet.next()){
+                    Log.d("appTAG", "data: " + resultSet.getString(1) + ", " + resultSet.getString(2));
+                    textView.setText(resultSet.getString(1));
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         }
-        else{
-            ConnectionResult = "Check Connection";
-        }
-        }
-        catch (Exception ex){
-
+        else {
+            textView.setText("Connection is null");
         }
     }
 }
